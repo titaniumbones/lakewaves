e3fc8d85afc8498526befdbc267d5c7f9eaf2fff
Matt Price <matt.price@utoronto.ca>
2018-04-19 18:24:20 -0400

    Minor updates, mostly css & request promise

--- index.js
+++ index.js
@@ -1,51 +1,128 @@
 const download = require('images-downloader').images,
       fs = require('fs'),
-      request = require('request');
+      request = require('request-promise');
+
+const lakes = ["ontario", "erie", "huron", "superior", "michigan"];
+
+
+async function getLakeImages(lake) {
+  let waveDest = `images/${lake}/waves`,
+      windDest = `images/${lake}/wind`,
+      waveStem = "/" + lake[0] + "wv",
+      windStem = "/" + lake[0] + "wn",
+      baseURL = "http://www.glerl.noaa.gov/res/glcfs/";
+
+  for (var i = -48; i < 121; i++){
+    var offset = i;
+    if(offset > 9){
+      offset = "+"+offset;
+      cast="fcast";
+    }else if(offset > 0 && offset < 10){
+      offset = "+0"+offset;
+      cast="fcast";
+    }else if(offset < 0 && offset > -10){
+      offset = "-0"+(i*(-1));
+      cast="ncast";
+    }else if(offset == 0){ //skip zero, avoid preload issues
+      offset = "-00";
+      //i=1;
+      cast="ncast";
+    } else {
+      cast = "ncast";
+    }
+
+    await request(baseURL + cast + waveStem + offset + ".gif")
+      .pipe(fs.createWriteStream(waveDest + waveStem + offset + ".gif"));
+    //console.log(req);
+    //req.pipe(fs.createWriteStream(waveDest + waveStem + offset + ".gif"));
+    console.log(waveDest + waveStem + offset + ".gif");
+    await request(baseURL + cast + windStem + offset + ".gif")
+      .pipe(fs.createWriteStream(windDest + windStem + offset + ".gif"));
+    //promises.push(req);
+    //console.log (`downloaded ${url} to ${image}`);
+    
+    // for (let stem of ["/owv", "/own"]) {
+    //   let url = "http://www.glerl.noaa.gov/res/glcfs/"+cast+ stem + offset+".gif",
+    //       image = "";
+    //   if (stem == "/owv") {
+    //     image = waveDest + stem + offset + ".gif";
+    //   } else {
+    //     image = windDest + stem + offset + ".gif";
+    //   }
+      
+    //   let req = request(url);
+    //   req.pipe(fs.createWriteStream(image));
+    //   //promises.push(req);
+    //   console.log (`downloaded ${url} to ${image}`);
+    // }
+    
+    wave_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/owv"+ offset+".gif");
+    wind_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/own"+ offset+".gif");
+  }  
+}
 
 // The file will be downloaded to this directory. For example: __dirname + '/mediatheque'
-const waveDest = 'images/waves',
-      windDest = 'images/wind';
-
 let wave_images = [],
     wind_images = [],
     cast = "";
 
-for (var i = -48; i < 121; i++){
-  var offset = i;
-  if(offset > 9){
-    offset = "+"+offset;
-    cast="fcast";
-  }else if(offset > 0 && offset < 10){
-    offset = "+0"+offset;
-    cast="fcast";
-  }else if(offset < 0 && offset > -10){
-    offset = "-0"+(i*(-1));
-    cast="ncast";
-  }else if(offset == 0){ //skip zero, avoid preload issues
-    offset = "+01";
-    i=1;
-    cast="fcast";
-  } else {
-    cast = "ncast";
-  }
-  for (let stem of ["/owv", "/own"]) {
-    let url = "http://www.glerl.noaa.gov/res/glcfs/"+cast+ stem + offset+".gif",
-        image = "";
-    if (stem == "/owv") {
-      image = "images/waves/" + stem +offset + ".gif";
-    } else {
-      image = "images/wind/" + stem +offset + ".gif";
-    }
-      
-    let req = request(url);
-    req.pipe(fs.createWriteStream(image));
-    //promises.push(req);
-    console.log (`downloaded ${url} to ${image}`);
+
+lakes.forEach(function(lake) {getLakeImages(lake)});
+
+/**
+ * 
+ * @param {} lake and object with properties name, slug, assets?
+ */
+function buildLakeTab (lake) {
+  
+}
+
+/**
+ * 
+ * @param {} lakes list of lake objects
+ */
+function buildNav (lakes) {
+  let navWrapper = {
+    open: `<ul class="nav nav-tabs" id="myTab" role="tablist">`,
+    close: `</ul>`
+  };
+  let navHTML = navWrapper.open;
+  for (l of lakes) {
+    
   }
   
-  wave_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/owv"+ offset+".gif");
-  wind_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/own"+ offset+".gif");
 }
+
+/**
+*
+*
+**/
+function buildIndexHTML( ) {
+
+  // assemble navs
+  let navsHTML = buildNav(lakes)
+  
+  // assemble tabs
+  let tabsWrapper = {
+    open: `<div class="tab-content" id="myTabContent">`,
+    close: `</div>`
+  };
+  let tabsHTML = tabsWrapper.open;
+  for (l of lakes) {
+    tabsHTML += buildLakeTab(l);
+  }
+  tabsHTML += tabsWrapper.close;
+
+  
+}
+
+
+
+
+
+
+
+
 
 
 
--- slider.js
+++ slider.js
@@ -1,13 +1,15 @@
-var the_images = document.the_images = [];
+let the_images = document.the_images = [];
+const lakes = ["ontario", "erie", "huron", "superior", "michigan"];
+
 
 
 
 $.preloadImages = function(waves, wind) {
   var loaded = 0;
-  var total = 168 *2;
-  for (w of [waves, wind]) {
+  var total = 169 *2 *5;
+  for (let w of [waves, wind]) {
     for (let i of w) {
-      let attrs = {"data-gallery": "wind",
+      let attrs = {//"data-gallery": "wind",
                    "data-toggle": "lightbox",
                    "href": i};
       let a= $('<a>');
@@ -29,30 +31,34 @@ $(function() {
   let wave_images = [],
       wind_images = [],
       cast = "";
-  for (var i = -48; i < 121; i++){
-	var offset = i;
-	if(offset > 9){
-		offset = "+"+offset;
-		cast="fcast";
-	}else if(offset > 0 && offset < 10){
-		offset = "+0"+offset
-				cast="fcast";
-	}else if(offset < 0 && offset > -10){
-		offset = "-0"+(i*(-1));
-		cast="ncast";
-	}else if(offset == 0){ //skip zero, avoid preload issues
-		offset = "+01";
-		i=1;
-		cast="fcast";
-	} else {
-          cast = "ncast";
-        }
-    the_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/owv"+ offset+".gif");
-    the_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/own"+ offset+".gif");
-    wave_images.push( "images/waves/owv"+ offset+".gif");
-    wind_images.push( "images/wind/own"+ offset+".gif");
 
+  for (let lake of lakes) {
+    let l = lake[0];
+    for (var i = -48; i < 121; i++){
+      var offset = i;
+      if(offset > 9){
+	offset = "+"+offset;
+	cast="fcast";
+      }else if(offset > 0 && offset < 10){
+	offset = "+0"+offset;
+	cast="fcast";
+      }else if(offset < 0 && offset > -10){
+	offset = "-0"+(i*(-1));
+	cast="ncast";
+      }else if(offset == 0){ //skip zero, avoid preload issues
+	offset = "-00";
+	cast="ncast";
+      } else {
+        cast = "ncast";
+      }
+      the_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/owv"+ offset+".gif");
+      the_images.push("http://www.glerl.noaa.gov/res/glcfs/"+cast+"/own"+ offset+".gif");
+      wave_images.push( `images/waves/${l}wv${offset}.gif`);
+      wind_images.push( `images/wind/${l}wn${offset}.gif`);
+
+    }
   }
+  
   
   
   //$.preloadImages(the_images);
@@ -63,7 +69,7 @@ $(function() {
       range: "min",
       min: -48,
       max: 120,
-      value: 01,
+      value: 0,
 
       slide: function( event, ui ) {
 	var offset = ui.value;
@@ -71,13 +77,13 @@ $(function() {
 	  offset = "+"+offset;
 	  cast="fcast";
 	}else if(offset > 0 && offset < 10){
-	  offset = "+0"+offset
+	  offset = "+0"+offset;
 	  cast="fcast";
 	}else if(offset < 0 && offset > -10){
 	  offset = "-0"+(offset*(-1));
 	  cast="ncast";
 	}else if(offset == 0){ //skip zero, avoid preload issues
-	  offset = "+01";
+	  offset = "-00";
 	  cast="fcast";
 	}
 
@@ -85,42 +91,26 @@ $(function() {
         // $("#waves").attr("src","http://www.glerl.noaa.gov/res/glcfs/"+cast+"/owv"+ offset +".gif");
         // $("#wind").attr("src","http://www.glerl.noaa.gov/res/glcfs/"+cast+"/own"+ offset +".gif"); 
         // $("#wave-link").attr("href","http://www.glerl.noaa.gov/res/glcfs/"+cast+"/owv"+ offset +".gif");
-        $("#waves").attr("src","images/waves/owv"+ offset +".gif");
-        $("#wind").attr("src","images/wind/own"+ offset +".gif"); 
-        $("#wave-link").attr("href","images/waves/owv"+ offset +".gif");
-        $("#wind-link").attr("href","images/wind/own"+ offset +".gif");
-
+        for (let lake of lakes) {
+          let l=lake[0];
+          $(`#${lake}-waves`).attr("src",`images/${lake}/waves/${l}wv${offset}.gif`);
+          $(`#${lake}-wind`).attr("src",`images/${lake}/wind/${l}wn${offset}.gif`); 
+          $(`#${lake}-wave-link`).attr("href",`images/${lake}/waves/${l}wv`+ offset +`.gif`);
+          $(`#${lake}-wind-link`).attr("href",`images/${lake}/wind/${l}wn`+ offset +`.gif`);
+        }
       }
     });    
 
-    $("#waves").attr("src","http://www.glerl.noaa.gov/res/glcfs/fcast/owv+01.gif");
-    $("#wind").attr("src","http://www.glerl.noaa.gov/res/glcfs/fcast/own+01.gif");        
-    
-    //$("#graph_container").fadeIn(1000);
 
-  
-    // jQuery.imgpreload(the_images,
-    //     	      {
-    //     		each: function()
-    //     		{
-    //     		  loaded++;
-    //     	          var percentage = parseInt(loaded / total * 100) + '%';
-    //                       $('#progress-bar').html("Loading images: "+percentage);
-    //     		},
-    //     		all: function()
-    //     		{
-    //                       $('#progress-bar').fadeOut(300);
+  for (let lake of lakes) {
+    let l=lake[0];
+    $(`#${lake}-waves`).attr("src",`images/${lake}/waves/${l}wv-00.gif`);
+    $(`#${lake}-wind`).attr("src",`images/${lake}/wind/${l}wn-00.gif`); 
+    $(`#${lake}-wave-link`).attr("href",`images/${lake}/waves/${l}wv-00.gif`);
+    $(`#${lake}-wind-link`).attr("href",`images/${lake}/wind/${l}wn-00.gif`);
+  }
 
-    //     		  //enable slider
-    //     		  $( "#slider" ).slider("enable");
-			  
-    //     		  //load images for +01 (fade in?)
-	                  
-    //     		}
-    //     	      });
-    
+  //document.getElementById("slider").focus();
 
-
-    
-    
+  $("#slider ui-slider-handle").trigger('click');
   });
