3c86d58d2dc9cb8f5a1d03cef0e65ded5460123c
Matt Price <matt.price@utoronto.ca>
2018-09-13 09:06:51 -0400

    initial refactor
    Added some improved funcitons, with lots of cleanup still required.
    
    - `constructiImageList` and `constructFullImageList` allow the
      imageMeta file to be reconstituted from scratch.
    - `writeImageMetaToFile` isn't being used yet, but it will write an
      imageMeta file for us.
    - `gitImage` is now the main async function, it both fetches an image
      and updates imageMeta description of it (but doesn't write to disk)
    - `getAllLakes` now just calls `getImage` on every entry in imageMeta;
    
    **TODO** allow more granular downloading if you don't want all images.

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ const lakes = ["ontario", "erie", "huron", "superior", "michigan"];
 
 let imageMeta= jsonfile.readFileSync(`images/imageMeta.json`),
     errors = 1;
-;
+const defaultModTime = "Sat, 1 Sep 2018 01:12:51 GMT";
 
-async function getLakeImages(lake) {
+
+function constructImageList(lake) {
+  
   let waveDest = `images/${lake}/waves`,
       windDest = `images/${lake}/wind`,
       waveStem = "/" + lake[0] + "wv",
       windStem = "/" + lake[0] + "wn",
       baseURL = "http://www.glerl.noaa.gov/res/glcfs/",
-      cast="";
+      cast="",
+      meta={};
       
   for (var i = -48; i < 121; i++){
     var offset = i;
@@ -43,6 +46,100 @@ async function getLakeImages(lake) {
         wind = {name: windStem + offset,
                 fullURL: baseURL + cast + windStem + offset + ".gif",
                 fullPath: windDest + windStem + offset + ".gif"};
+    meta[wave.name] = {lastModified : defaultModTime,
+                       path: wave.fullPath,
+                       url: wave.fullURL};
+    meta[wind.name] = {lastModified : defaultModTime,
+                       path: wind.fullPath,
+                       url: wind.fullURL};
+
+  }
+return meta;
+}
+
+function constructFullImageList() {
+  let result = {};
+  for (l of lakes) {
+    Object.assign(result, constructImageList(l));
+  }
+  writeImageMetaToFile("testing.json", result);
+  return result;
+}
+
+function writeImageMetaToFile(file, obj) {
+  jsonfile.writeFile(file, obj, function (err) {
+    if (err) console.error(err)
+  });
+  // jsonfile.writeFile(file, obj)
+  //   .then(res => {
+  //     console.log('Write complete');
+  //   })
+  //   .catch(error => console.error(error));
+}
+
+// takes a single meta object and returns a modified version of it
+// if successful, otherwise returns an error object
+async function getImage(imageName) {
+  let singleMeta = imageMeta[imageName];
+  
+  let path = singleMeta.path,
+      modTime = singleMeta.lastModified,
+      url = singleMeta.url;
+  
+  let options = {url: singleMeta.url,
+                 method: "GET",
+                 resolveWithFullResponse: true,
+                 headers:{'If-Modified-Since': singleMeta["lastModified"],
+                          'fulldownloadpath':singleMeta.path},
+                 encoding: null,
+                 simple: false,
+                 testoptions: "testingnow"
+                };
+  await request(options)
+        .then(function(response){
+          if (response.statusCode == 200) {
+            console.log(`resolved ${url} successfully!`);
+            console.log(`attempting to write to ${path}`);
+            let thisStream = fs.createWriteStream(path);
+            // console.log(response);
+            thisStream.write(response.body);
+            thisStream.end();
+            thisStream.on('finish', () => {
+              console.log(`wrote all data to ${path}`);
+            });
+            singleMeta.lastModified = response.headers['last-modified'];
+            imageMeta[imageName] = singleMeta;
+            //console.log(imageMeta[imageName]);
+            // imageMeta[w.name] = {url: w.fullURL,
+            //                      path: w.fullPath,
+            //                      lastModified: response.headers['last-modified']};
+            // console.log(imageMeta[w.name]);
+          } else if (response.statusCode == 304) {
+            console.log (`${w.name} is unchanged and does not need to be downloaded.`)
+          } else {
+            console.log(`doesn't look good. ${w.name} returned ${response.statusCode}`);
+            errors += 1;
+          }
+        })
+        .catch (function (err) {
+          console.log(`oops, error! ${err}`);
+          console.dir(w);
+          errors += 1;
+
+        });
+
+  return singleMeta;
+      // try {
+      //   let r = await request(options);
+        
+      // } catch (err) {
+
+      // }
+    }
+
+async function getLakeImages(lake) {
+      
+  for (var i = -48; i < 121; i++) {
     for (w of [wave,wind] ) {
       let options = {url: w.fullURL,
                      method: "GET",
@@ -119,11 +216,11 @@ async function allLakes () {
 
   request(options)
     .then(function(results) {
-      
-      if (results.headers.statusCode == 200 ) {
+      console.log(results.statusCode)
+      if (results.statusCode == 200 ) {
         let allPromises = [];
-        for (let lake of lakes) {
-          allPromises.push (getLakeImages(lake));
+        for (let i in imageMeta) {
+          allPromises.push (getImage(i));
         }
         Promise.all(allPromises)
           .then(function (values) {
@@ -144,14 +241,8 @@ async function allLakes () {
     });
 
 }
-allLakes()
-  // .then(function (){
-  //   console.dir(imageMeta);
-  //   console.log(`there were ${errors} errors loading the files.`);
-  //   jsonfile.writeFile('images/imageMeta.json', imageMeta, function (err) {
-  //     console.log(`unable to write to images/imageMeta.json: ${err}`);
-  //   });
-  // });
+
+
 
 /**
  * 
@@ -200,13 +291,27 @@ function buildIndexHTML( ) {
   
 }
 
+
+
 // for (let lake of lakes) {
 //   getLakeImages(lake);
 // }
+// testImage =  { lastModified: 'Sat, 1 Sep 2018 01:12:51 GMT',
+//                path: 'images/michigan/wind/mwn+120.gif',
+//                url: 'http://www.glerl.noaa.gov/res/glcfs/fcast/mwn+120.gif' };
+// testImage = "/mwn+120";
 
+// console.log(getImage(testImage));
+// console.log(constructFullImageList());
 
-
-
+allLakes()
+// .then(function (){
+//   console.dir(imageMeta);
+//   console.log(`there were ${errors} errors loading the files.`);
+//   jsonfile.writeFile('images/imageMeta.json', imageMeta, function (err) {
+//     console.log(`unable to write to images/imageMeta.json: ${err}`);
+//   });
+// });
 
 
 
